be19dec94932bb02ca41a4e1b070b5dae07ee3ea
EricJess2001 <ericjessid2018@gmail.com>
2023-04-16 15:43:25 -0500

    arreglo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState} from 'react';
 
 function App() {
+
+  const [file, setFile] = useState(null)
+
+  const selectedHander = e => {
+    setFile(e.target.files[0])
+   }
+ 
+   const sendHandler = () => {
+    if(!file){
+      alert('debes subir algun archivo')
+      return
+    }
+
+    const formdata = new FormData()
+    formdata.append('image', file)
+
+    fetch('http://localhost:9000/images/post',{
+      method: 'POST',
+      body: formdata
+    })
+    .then(res => res.text())
+    .then(res => console.log(res))
+    .catch(err => {
+      console.error(err)
+    })
+
+    document.getElementById('fileinput').value = null
+
+    setFile(null)
+
+   }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <Fragment>
+ <nav className="navbar navbar-dark bg-dark">  
+   <div className="container"> 
+      <a href="#!" className="navbar-brand">Cultupaz</a>
+   </div>
+ </nav>
+ <br>
+ </br>
+ <div className="container mt-5">
+  <div className="card p-3">
+    <div className="row">
+      <div className="col-10">
+      <input id="fileinput" onChange={selectedHander} className="form-control" type="file"/>
+      </div>
+      <div className="col-2">
+      <button onClick={sendHandler} type="button" className="btn btn-primary col-12">Subir</button>
+      </div>
     </div>
+  </div>
+ </div>
+
+ </Fragment>
+
+
   );
 }
 
