b536834cfb10b099aa108a1640171ec79d2ac816
EricJess2001 <ericjessid2018@gmail.com>
2023-05-06 19:14:48 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, {Fragment, useState, useEffect} from 'react';
+import Modal from 'react-modal'
 
 function App() {
 
   const [file, setFile] = useState(null)
   const [imageList, setImageList] = useState([])
   const [ListUpdated, setListUpdated] = useState(false)
+
+  const [currentImage, setCurrentImage] = useState(null)
+
+
+  const [modalLisOpen, setModaLisOpen] = useState(false)
+
+
+
+
+  useState(() => {
+
+
+  }
+  
+  )
    
 
    useEffect(() => {
+
+     
+    Modal.setAppElement('body')
+
      fetch('http://localhost:9000/images/get')
     .then(res => res.json())
     .then(res => setImageList(res))
@@ -50,7 +70,32 @@ function App() {
 
    }
 
+   const modalHandler = (isOpen, image) => {
 
+       setModaLisOpen(isOpen)
+       setCurrentImage(image)
+
+
+   }
+
+   const deleteHandler = () => {
+
+     let   imageID = currentImage.split('-')
+     imageID = parseInt(imageID[0])
+
+     fetch('http://localhost:9000/images/delete/' + imageID,{
+      method: 'DELETE'
+   })
+         .then(res => res.text())
+         .then(res => console.log(res))
+
+
+         setModaLisOpen(false)
+         setListUpdated(true)
+
+
+
+  }
   return (
  <Fragment>
  <nav className="navbar navbar-dark bg-dark">  
@@ -74,14 +119,30 @@ function App() {
   </div>
  </div>
 
- <div className="container mt-4" style={{display: "flex", flexWrap: "wrap"}}>
+ <div className="container mt-3" style={{display: "flex", flexWrap: "wrap"}}>
      {imageList.map(image => (
       <div key={image}  className="card m-2">
       <img src={'http://localhost:9000/' + image} alt="..." className="card-img-top" style={{height: "200px", width: "261px"}}/>
+      <div className='card-body'>
+        <button onClick={() => modalHandler (true, image)} className='btn btn-dark'>Ver mas</button>
+
+        </div>
     </div>
      ))}
 
  </div>
+
+ <Modal  style={{content: {right: "5%", left: "5%"}}}  isOpen={modalLisOpen} onRequestClose={() => modalHandler(false, null)}>
+
+  <div className='card'>
+     <img src={'http://localhost:9000/' + currentImage} alt='...'/>
+     <div className='card-body'>
+        <button onClick={() => deleteHandler()} className='btn btn-warning'>Eliminar</button>
+    
+        </div>
+  </div>
+
+ </Modal>
 
  </Fragment>
 
